5f33c5312af9b93f682769ccf985e659113d853e
Jesse Rosendahl <jesse@jessar.nl>
2021-11-14 14:07:25 +0100

    added random time

--- src/Explosion.hs
+++ src/Explosion.hs
@@ -18,20 +18,22 @@ data Explosion = Explosion
 stepExplosion :: Float -> Explosion -> IO [Explosion]
 stepExplosion dt (Explosion p (Animation f t) l)
   | t > 0.05 && f == 4 = return []
-  | t > 0.05 = do
+  | t > 0.05 && l > 0 = do
     newExplosion <- spawnExplosion (l Prelude.- 1) p
     let currentExplosion = Explosion p (Animation (f Prelude.+ 1) 0) (l Prelude.- 1)
-    if l > 0
-    then return [newExplosion,currentExplosion]
-    else return [currentExplosion]
+    return [newExplosion,currentExplosion]
+  | t > 0.05 = return [Explosion p (Animation (f Prelude.+ 1) 0) 0]
   | otherwise = return [Explosion p (Animation f (t Prelude.+ dt)) l]
 
 spawnExplosion :: Int -> Vector -> IO Explosion
 spawnExplosion amount position = do
   radius <- randomIO :: IO Float
   angle <- randomIO :: IO Float
+  randomT <- randomIO :: IO Float
+  let randomTime = randomT Prelude.* 0.1
   let offset = (cos (angle Prelude.* pi Prelude.* pi),sin (angle Prelude.* pi Prelude.* 2))
-  return $ Explosion (position Vector.+ ((radius Prelude.* 8) Vector.* offset)) (Animation 0 0) amount
+  return $ Explosion (position Vector.+ ((radius Prelude.* 8) Vector.* offset)) (Animation 0 (Prelude.negate randomTime)) amount
 
 viewExplosion :: Assets -> Explosion -> Picture
 viewExplosion assets (Explosion p (Animation f _) _) = uncurry translate p (explosionSprites assets !! f)
+
